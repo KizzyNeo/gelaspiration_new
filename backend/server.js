87bccaa2ab480bd8aeea4f69659f6bd6a6668777
Kizito Ajaereh <108177298+KizzyNeo@users.noreply.github.com>
2026-03-28 20:21:45 +0100

    Modify CORS settings to include specific origin
    Updated CORS configuration to allow requests from the specified origin.

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://gelaspirationnew.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static("uploads"));
+// app.use("/uploads", express.static("uploads"));
 
 const contestantRoutes = require("./routes/contestantsRoutes");
 app.use("/api/contestants", contestantRoutes);
