a7d82f863f607810cd2901409d90845b55de7005
Kizito Ajaereh <108177298+KizzyNeo@users.noreply.github.com>
2026-04-10 09:34:44 +0100

    Enhance login with loading state and form submission
    Added loading state to the login process and updated the form submission to use a form element.

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,26 +3,36 @@ import axios from "axios";
 
 function Login() {
     const [form, setForm] = useState({ username: "", password: "" });
-
+    const [loading, setLoading] = useState(false);
     const currentYear = new Date().getFullYear();
 
     const handleLogin = async () => {
+        setLoading(true);
         try {
             const res = await axios.post("https://api.gelaspiration.com/api/auth/login", form);
             localStorage.setItem("token", res.data.token);
             window.location.href = "/admin";
         } catch {
             alert("Invalid login");
+        } finally {
+            setLoading(false);
         }
     };
 
     return (
-        <div id="login_form">
+        <div className="order">
             <img src={require("../assets/logo.png")} alt="gelaspiration_logo" className="brown_logo" />
-            <h2 id="login">Admin Login</h2>
-            <input placeholder="Username" onChange={e => setForm({...form, username: e.target.value})} className="login_input" />
-            <input type="password" placeholder="Password" onChange={e => setForm({...form, password: e.target.value})} className="login_input" />
-            <button className="btn-three" onClick={handleLogin}>Login</button>
+            <h2 id="login">Admin Login (SBC)</h2>
+            <form onSubmit={handleLogin} className="form">
+                <input placeholder="Username" onChange={e => setForm({...form, username: e.target.value})} />
+                <input type="password" placeholder="Password" onChange={e => setForm({...form, password: e.target.value})} />
+                <button type="submit" className="btn-three" disabled={loading} style={{ display: "flex" }}>{loading ? (
+                    <>
+                    Logging In...<div className="spinnerStyle"></div>
+                    </>
+                    ) : ( "Login" )}
+                </button>
+            </form>
             <div className="general_footer">
                 <small>&copy; <span>{currentYear}</span> Sing Better Competition (SBC). All rights reserved.</small>
             </div>
