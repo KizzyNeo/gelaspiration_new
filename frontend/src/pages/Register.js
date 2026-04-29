3a9df460a2ff147f08e612413f2e32e388d94567
Kizito Ajaereh <108177298+KizzyNeo@users.noreply.github.com>
2026-04-29 11:45:54 +0100

    Modify Register component for registration closure
    Updated the Register component to include a closure alert for registration status and commented out the form elements.

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -65,74 +65,75 @@ function Register() {
                             <meta property="og:url" content="https://gelaspiration.com" />
                             <meta name="twitter:card" content="summary_large_image" />
                         </Helmet>
-            <img src={require("../assets/logo.png")} alt="gelaspiration_logo" className="brown_logo" />
-            <div id="instruction">
-                <h3 id="info2">Note:</h3>
-                <p id="info2_text">
-                    After entering your details, click on the "Submit" button, 
-                    wait for it to finish, leave your browser open and you will be REDIRECTED automatically to the official 
-                    WhatsApp group for the contest. Please follow the on-screen 
-                    instructions of your browser to join the WhatsApp group. Success!
-                </p>
-            </div>
-            <h2 id="reg">Register for SBC 7.0</h2>
-            <form onSubmit={handleSubmit} className="form">
-                <input type="text" name="name" placeholder="Full Name" onChange={handleChange} required />
-                <select name="gender" onChange={handleChange} required>
-                    <option value="">Select Gender</option>
-                    <option value="Male">Male</option>
-                    <option value="Female">Female</option>
-                </select>
-                <select id="state" name="location" onChange={handleChange} required>
-                    <option value="">--Select a State--</option>
-                    <option value="Abia">Abia</option>
-                    <option value="Adamawa">Adamawa</option>
-                    <option value="Akwa Ibom">Akwa Ibom</option>
-                    <option value="Anambra">Anambra</option>
-                    <option value="Bauchi">Bauchi</option>
-                    <option value="Bayelsa">Bayelsa</option>
-                    <option value="Benue">Benue</option>
-                    <option value="Borno">Borno</option>
-                    <option value="Cross River">Cross River</option>
-                    <option value="Delta">Delta</option>
-                    <option value="Ebonyi">Ebonyi</option>
-                    <option value="Edo">Edo</option>
-                    <option value="Ekiti">Ekiti</option>
-                    <option value="Enugu">Enugu</option>
-                    <option value="FCT">Federal Capital Territory</option>
-                    <option value="Gombe">Gombe</option>
-                    <option value="Imo">Imo</option>
-                    <option value="Jigawa">Jigawa</option>
-                    <option value="Kaduna">Kaduna</option>
-                    <option value="Kano">Kano</option>
-                    <option value="Katsina">Katsina</option>
-                    <option value="Kebbi">Kebbi</option>
-                    <option value="Kogi">Kogi</option>
-                    <option value="Kwara">Kwara</option>
-                    <option value="Lagos">Lagos</option>
-                    <option value="Nasarawa">Nasarawa</option>
-                    <option value="Niger">Niger</option>
-                    <option value="Ogun">Ogun</option>
-                    <option value="Ondo">Ondo</option>
-                    <option value="Osun">Osun</option>
-                    <option value="Oyo">Oyo</option>
-                    <option value="Plateau">Plateau</option>
-                    <option value="Rivers">Rivers</option>
-                    <option value="Sokoto">Sokoto</option>
-                    <option value="Taraba">Taraba</option>
-                    <option value="Yobe">Yobe</option>
-                    <option value="Zamfara">Zamfara</option>
-                </select>
-                <input type="text" name="phone" placeholder="Phone" onChange={handleChange} required />
-                <label className="info">Professional Photo:</label>
-                <input type="file" name="photo" id="photo" onChange={handleChange} required />
-                <button type="submit" className="btn-three" disabled={loading} style={{ display: "flex" }}>{loading ? (
-                    <>
-                    Registering...<div className="spinnerStyle"></div>
-                    </>
-                ) : ( "Submit" )}
-                </button>
-            </form>
+                        alert("Registration is closed.");
+            // <img src={require("../assets/logo.png")} alt="gelaspiration_logo" className="brown_logo" />
+            // <div id="instruction">
+            //     <h3 id="info2">Note:</h3>
+            //     <p id="info2_text">
+            //         After entering your details, click on the "Submit" button, 
+            //         wait for it to finish, leave your browser open and you will be REDIRECTED automatically to the official 
+            //         WhatsApp group for the contest. Please follow the on-screen 
+            //         instructions of your browser to join the WhatsApp group. Success!
+            //     </p>
+            // </div>
+            // <h2 id="reg">Register for SBC 7.0</h2>
+            // <form onSubmit={handleSubmit} className="form">
+            //     <input type="text" name="name" placeholder="Full Name" onChange={handleChange} required />
+            //     <select name="gender" onChange={handleChange} required>
+            //         <option value="">Select Gender</option>
+            //         <option value="Male">Male</option>
+            //         <option value="Female">Female</option>
+            //     </select>
+            //     <select id="state" name="location" onChange={handleChange} required>
+            //         <option value="">--Select a State--</option>
+            //         <option value="Abia">Abia</option>
+            //         <option value="Adamawa">Adamawa</option>
+            //         <option value="Akwa Ibom">Akwa Ibom</option>
+            //         <option value="Anambra">Anambra</option>
+            //         <option value="Bauchi">Bauchi</option>
+            //         <option value="Bayelsa">Bayelsa</option>
+            //         <option value="Benue">Benue</option>
+            //         <option value="Borno">Borno</option>
+            //         <option value="Cross River">Cross River</option>
+            //         <option value="Delta">Delta</option>
+            //         <option value="Ebonyi">Ebonyi</option>
+            //         <option value="Edo">Edo</option>
+            //         <option value="Ekiti">Ekiti</option>
+            //         <option value="Enugu">Enugu</option>
+            //         <option value="FCT">Federal Capital Territory</option>
+            //         <option value="Gombe">Gombe</option>
+            //         <option value="Imo">Imo</option>
+            //         <option value="Jigawa">Jigawa</option>
+            //         <option value="Kaduna">Kaduna</option>
+            //         <option value="Kano">Kano</option>
+            //         <option value="Katsina">Katsina</option>
+            //         <option value="Kebbi">Kebbi</option>
+            //         <option value="Kogi">Kogi</option>
+            //         <option value="Kwara">Kwara</option>
+            //         <option value="Lagos">Lagos</option>
+            //         <option value="Nasarawa">Nasarawa</option>
+            //         <option value="Niger">Niger</option>
+            //         <option value="Ogun">Ogun</option>
+            //         <option value="Ondo">Ondo</option>
+            //         <option value="Osun">Osun</option>
+            //         <option value="Oyo">Oyo</option>
+            //         <option value="Plateau">Plateau</option>
+            //         <option value="Rivers">Rivers</option>
+            //         <option value="Sokoto">Sokoto</option>
+            //         <option value="Taraba">Taraba</option>
+            //         <option value="Yobe">Yobe</option>
+            //         <option value="Zamfara">Zamfara</option>
+            //     </select>
+            //     <input type="text" name="phone" placeholder="Phone" onChange={handleChange} required />
+            //     <label className="info">Professional Photo:</label>
+            //     <input type="file" name="photo" id="photo" onChange={handleChange} required />
+            //     <button type="submit" className="btn-three" disabled={loading} style={{ display: "flex" }}>{loading ? (
+            //         <>
+            //         Registering...<div className="spinnerStyle"></div>
+            //         </>
+            //     ) : ( "Submit" )}
+            //     </button>
+            // </form>
             <div className="general_footer">
                 <small>&copy; <span>{currentYear}</span> Sing Better Competition (SBC). All rights reserved.</small>
             </div>
